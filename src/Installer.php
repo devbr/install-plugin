4cfd370f7cdb436a0f5159de35d3c941f2ca7ca3
BlackScorpion <prbr@ymail.com>
2017-08-09 21:45:52 -0300

    Teste copy Config to appConfig

--- src/Installer.php
+++ src/Installer.php
@@ -51,6 +51,88 @@ class Installer extends LibraryInstaller
         }
         
         //ME Code ----
-        if(file_exists($downloadPath.'/Config') && is_read...
+        $packConfig = $downloadPath.'/Config';
+        $appConfig =  $this->baseDir.'/.php/Config';
+
+        echo "\n\n\tPackConfig: $packConfig\n\tAppConfig: $appConfig\n\n";
+
+        if(file_exists($packConfig) && is_readable($packConfig)){
+            echo "\n\t--- Config exists ---";
+            self::checkAndOrCreateDir($appConfig, true);
+            self::copyDirectoryContents($packConfig, $appConfig);
+        }
+    }
+
+
+    /**
+     * Check or create a directory
+     * @param  string  $dir    path of the directory
+     * @param  boolean $create False/true for create
+     * @param  string  $perm   indiucates a permission - default 0777
+     *
+     * @return bool          status of directory (exists/created = false or true)
+     */
+    static private function checkAndOrCreateDir($dir, $create = false, $perm = 0777)
+    {
+        if (is_dir($dir) && is_writable($dir)) {
+            return true;
+        } elseif ($create === false) {
+            return false;
+        }
+
+        @mkdir($dir, $perm, true);
+        @chmod($dir, $perm);
+
+        if (!is_writable($dir)) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    /**
+     * Copy entire content of the $dir[ectory]
+     * @param  string $dir    Origin
+     * @param  string $target Destination
+     * @return bool         True/false success
+     */
+    static private function copyDirectoryContents($dir, $target, $overwrite = true, $base = '')
+    {
+        $dir = rtrim($dir, "\\/ ").'/';
+        $target = rtrim($target, "\\/ ").'/';
+        $report = ['error'=>[],'copied'=>[]];
+
+        if (!static::checkAndOrCreateDir($target, true, 0777)) {
+            $report['error']['permission'] = $taget;
+            return $report;
+        }
+
+        foreach (scandir($dir) as $file) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+
+            if (is_dir($dir.$file)) {
+                if (!static::checkAndOrCreateDir($target.$file, true, 0777)) {
+                    $report['error']['permission'] = $taget.$file;
+                    return $report;
+                } else {
+                    $copy = static::copyDirectoryContents($dir.$file, $target.$file, $overwrite, $base);
+                    $report = array_merge_recursive($report, $copy);
+                }
+            } elseif (is_file($dir.$file)) {
+                if ($overwrite === false && file_exists($target.$file)) {
+                    $report['error']['overwrite'][] = str_replace($base.'/', '', $target.$file);
+                    continue;
+                }
+                if (!copy($dir.$file, $target.$file)) {
+                    $report['error']['permission'] = $target.$file;
+                    return $report;
+                } else {
+                    $report['copied'][] = str_replace($base.'/', '', $target.$file);
+                }
+            }
+        }
+        return $report;
     }
 }
